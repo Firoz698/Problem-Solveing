fc0721c9a5610391357bf0ecfcdd0d41db5a08ae
Itz Firoz <mdfirozahemd698@gmail.com>
2023-12-31 01:29:15 +0600

    even and Odd

---
+++ ProjectOne/ClassOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOne
+{
+    internal class ClassOne
+    {
+
+    public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+
+    }
+}
---
+++ ProjectOne/Program.cs
@@ -0,0 +1,13 @@
+﻿//Find even or odd number in C# 
+
+Console.Write("Plase Enter Your Number : ");
+int Number =Convert.ToInt32(Console.ReadLine());
+
+if (Number%2==0)
+{
+    Console.WriteLine($"{Number} Even Number");
+}
+else
+{
+    Console.WriteLine($"{Number} Odd Number");
+}
