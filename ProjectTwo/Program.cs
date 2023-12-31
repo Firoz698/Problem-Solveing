854fbb89fe29c713fdc1a5b5a9a29467a4f7b91f
Itz Firoz <mdfirozahemd698@gmail.com>
2023-12-31 17:55:48 +0600

    1 to 100 even or odd number

--- ProjectTwo/Program.cs
+++ ProjectTwo/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello I am Firoz");
+﻿
+for (int i = 0; i <= 100; i++)
+{
+	if (i%2==0)
+	{
+        if (i==0)
+        {
+            continue;
+        }
+        else
+        {
+            Console.WriteLine(i + " Even Number ");
+        }
+    
+    }
+    else
+    {
+        Console.WriteLine(i +" Odd Number  ");
+    }
+}
+Console.WriteLine("Hello I am Firoz");
