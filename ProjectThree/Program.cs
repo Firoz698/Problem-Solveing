7fb1c27bb5b9f6319e36c67556457854dbc1726d
Itz Firoz <mdfirozahemd698@gmail.com>
2024-01-01 18:13:48 +0600

    Fibanocci List

---
+++ ProjectThree/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Plase Enter Your Fibanacci NO :");
+int Number =Convert.ToInt32( Console.ReadLine());
+int a = 0;
+int b = 1;
+int c;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+for (int i = 0; i < Number; i++)
+{
+    c = a + b;
+    Console.WriteLine(c);
+    a = b;
+    b = c;
+
+
+}
