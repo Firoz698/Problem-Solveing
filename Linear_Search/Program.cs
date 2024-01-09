d7d7e54f3ca8e15fdc9aa9a9a649f89f62821611
Itz Firoz <mdfirozahemd698@gmail.com>
2024-01-09 20:27:11 +0600

    linear search

---
+++ Linear_Search/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] myNumber = { 23, 32, 34, 45, 67, 88, 42, 12, 54 };
+Console.Write("Enter Your Number : ");
+int SerchNumber = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < myNumber.Length; i++)
+{
+    if (myNumber[i] == SerchNumber)
+    {
+        Console.WriteLine($"You Searching {myNumber[i]} ");
+    }
+}
