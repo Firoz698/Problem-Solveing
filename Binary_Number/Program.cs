aa349436413ef87d165864b2847cb754b9ccdecd
Itz Firoz <mdfirozahemd698@gmail.com>
2024-01-10 20:07:32 +0600

    binary search

---
+++ Binary_Number/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+
+
+Console.Write("Enter Your Number : ");
+int myNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = {34,54,66,78,12,21,46,90,34,23 };
+
+int Left, Right, Mid;
+Left = 0;
+Right = myArray.Length - 1;
+
+while (Left <= Right)
+{
+    Mid = (Left + Right) / 2;
+    if (myArray[Mid] == myNumber)
+    {
+        Console.WriteLine(Mid);
+    }
+    if (myArray[Mid] < myNumber)
+    {
+        Left = Left + 1;
+    }
+    else
+    {
+        Right = Right - 1;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+//for (int i = 0; i < myArray.Length; i++)
+//{
+//    if (myArray[i] != 0)
+//    {
+//        Console.WriteLine("Ok");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Linear_Seach/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Seach
+{
+    internal class Program
+    {
+
+
+        public static int BinaryMethod(int[] A,int n,int x)
+        {
+            int left, right, mid;
+            left = 0;
+            right = n - 1;
+
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+                if (A[mid] == x)
+                {
+                    return mid;
+                }
+                if (A[mid] < x)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;   
+        }
+        static void Main(string[] args)
+        {
+            int[] A = { 12, 22, 34, 23, 43, 54, 12, 34, 56 };
+            int n = A.Length;
+            Console.Write("Plase Enter Your Find Number :");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int data = BinaryMethod(A,n,x);
+            if(data < 0)
+            {
+                Console.WriteLine($"{x} Data Not Found");
+            }
+            else
+            {
+                Console.WriteLine(" Found Data : "+A[data]);
+            }
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
