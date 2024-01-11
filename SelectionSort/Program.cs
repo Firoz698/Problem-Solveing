203a26bb09c9c74d9695c7a610e5b27cca9df40b
Itz Firoz <mdfirozahemd698@gmail.com>
2024-01-11 23:38:15 +0600

    Baniray Search

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+
+int[] myArray = { 12,21,34,56,13,78,97,34,45};
+
+int store = 0;
+
+for (int i = 0;i <myArray.Length;i++)
+{
+    for (int j = 0;j < myArray.Length;j++)
+    {
+        if (myArray[i] < myArray[j] )
+        {
+            store = myArray[i];
+            myArray[i] = myArray[j];
+            myArray[j] = store;
+        }
+    }
+}
+foreach (int i in myArray)
+{
+    Console.Write(i+" ");
+}
